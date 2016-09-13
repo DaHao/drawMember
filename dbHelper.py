562de393ad20bf775b899577252590d21cfdbdc9
DaHao <jiash561@gmail.com>
2016-09-13 16:38:28 +0800

    add DB func

--- dbHelper.py
+++ dbHelper.py
@@ -2,6 +2,19 @@
 import sys
 import sqlite3
 import csv
+from flask import Flask, g
+
+app = Flask(__name__)
+SQLITE_DB_PATH = 'members.db'
+SQLITE_DB_SCHEMA = 'create_db.sql'
+MEMBER_CSV_PTAH = 'members.csv'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(SQLITE_DB_PATH)
+        db.execute("PRAGMA foreign_keys = ON")
+    return db
 
 def creatDB():
     with open('.\create_db.sql', encoding='utf-8') as f:
